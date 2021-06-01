268411a744e2c1f6f1a8929c3a8fba7c86574ae0
BVBROHIT1998 <46211619+BVBROHIT1998@users.noreply.github.com>
2021-06-01 20:42:37 +0530

    first Commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+require("./db/conn");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+const staticpath = path.join(__dirname, "../public");
+app.use('/css',express.static(path.join(__dirname,"../node_modules/bootstarp/dist/css")));
+app.use('/js',express.static(path.join(__dirname,"../node_modules/bootstarp/dist/js")));
+app.use('/jq',express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+app.use(express.static(staticpath))
+app.set("view engine", "hbs");
+
+//routing
+app.get("/contact",(req,res)=> {
+    res.render("contact");
+
+})
+
+//server create
+
+app.listen(port, ()=>{
+    console.log(`server is running at port no ${port}`);
+})
